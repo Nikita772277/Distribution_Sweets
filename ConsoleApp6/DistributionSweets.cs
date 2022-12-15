dadeaf28eba9cf3e380edd90f3b11cb42ee39aa5
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-15 12:49:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/DistributionSweets.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class DistributionSweets
+    {
+        public int DistributionOfSweets(int[] ints)
+        {
+            var counter = Counter(ints);
+            if (counter >= 2)
+            {
+                int Sweets = 0;
+                int maxSweets = 0;
+                foreach (int i in ints)
+                {
+                    if (maxSweets < i)
+                    {
+                        maxSweets = i;
+                    }
+                }
+                for (int i = 0; i < ints.Length; i++)
+                {
+                    if (ints[i] != maxSweets)
+                    {
+                        for (int j = 1; ;)
+                        {
+                            if (ints[i] + j == maxSweets)
+                            {
+                                Sweets += j;
+                                Console.WriteLine($"{i + 1} ребёнок получил {j} конфет");
+                                Console.WriteLine();
+                                break;
+                            }
+                            j++;
+                        }
+                    }
+                }
+                return Sweets;
+            }
+            return -1;
+        }
+        public int TotalChildren(int[] ints)
+        {
+            var counter = Counter(ints);
+            if (counter >= 1)
+            {
+                int totalChildren = 0;
+                foreach (var i in ints)
+                {
+                    totalChildren++;
+                }
+                return totalChildren;
+            }
+            return 0;
+        }
+        public void InitiallySweets(int[] ints)
+        {
+            string word = "";
+            for (var i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] == 1)
+                {
+                    word = "конфета";
+                }
+                else if (ints[i] == 2)
+                {
+                    word = "конфеты";
+                }
+                else
+                {
+                    word = "конфет";
+                }
+                Console.WriteLine($"Изначально у ребёнка {i + 1}-{ints[i]} {word}");
+                Console.WriteLine();
+            }
+        }
+        public int Counter(int[] ints)
+        {
+            int counter = 0;
+            for (int i = 0; i < ints.Length; i++)
+            {
+                counter++;
+                if (counter >= 2)
+                {
+                    return counter;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using ConsoleApp6;
+
+DistributionSweets ds = new DistributionSweets();
+Random random = new Random();
+var numberArray = random.Next(1, 20);
+int[] ints = new int[numberArray];
+for (int i = 0; i < numberArray; i++)
+{
+    var number = random.Next(1, 30);
+    ints[i] = number;
+}
+void Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine($"1)Всего дал конфет");
+    Console.WriteLine($"2)Всего детей");
+    Console.WriteLine($"3)Изначально конфет у каждого ребёнка");
+    Console.WriteLine();
+}
+void UseMenu()
+{
+    while (true)
+    {
+        Menu();
+        string enter = Console.ReadLine();
+        bool checkEnter = int.TryParse(enter, out int result);
+        if (result == 1)
+        {
+            var sumSweets = ds.DistributionOfSweets(ints);
+            Console.WriteLine();
+            if (sumSweets == -1)
+            {
+                Console.WriteLine($"");
+            }
+            else
+            {
+                Console.WriteLine($"Всего дал конфет: {sumSweets}");
+            }
+        }
+        else if (result == 2)
+        {
+            var numberChildren = ds.TotalChildren(ints);
+            Console.WriteLine();
+            if (numberChildren == -1)
+            {
+                Console.WriteLine($"");
+            }
+            else
+            {
+                Console.WriteLine($"Всего детей: {numberChildren}");
+            }
+        }
+        else if (result == 3)
+        {
+            ds.InitiallySweets(ints);
+        }
+        else
+        {
+            Console.WriteLine($"Выберите пункт из меню");
+        }
+    }
+
+}
+UseMenu();
+
+
